042d14effb54045753a82ce59c883f458b58a28b
c34333 <kouya.fujimaki@gmail.com>
2025-05-30 12:33:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -89,10 +89,14 @@ def handle_message(event):
     step = session.get("step")
 
     if step == "name":
-        session["name"] = text
-        reply(event.reply_token, f"{text}さん、こんにちは！")
-        session["step"] = "status"
-        send_quick_reply(event.reply_token, "① 案件進捗を選んでください", ["新規追加", "3:受注", "4:作業完了", "定期", "キャンセル"])
+    session["name"] = text
+    session["step"] = "status"
+    send_quick_reply(
+        event.reply_token,
+        f"{text}さん、こんにちは！\n① 案件進捗を選んでください",
+        ["新規追加", "3:受注", "4:作業完了", "定期", "キャンセル"]
+    )
+
     elif step == "status":
         session["status"] = text
         session["step"] = "company"
